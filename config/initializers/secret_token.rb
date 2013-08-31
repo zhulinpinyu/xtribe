ce3117be031eaa8ee67d97495405fe418d41be86
zhulinpinyu <mulixiang199016@gmail.com>
2013-09-01 07:53:48 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Xtribe::Application.config.secret_key_base = '50e94c8d120c0def7a1a0ce61baee5dc050af60984353379b95535d0210e19a025533aa06282380326a157594a405bf10c36b92b229c7b3b601efbe4183043bc'
